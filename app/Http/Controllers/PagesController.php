a6fbc4fc74008a83a5fad18c030149d34a3ec0b5
mmanzano <mmanzano@unifin.com.mx>
2018-09-11 13:56:40 -0500

    Se agrega por primera vez el proyecto

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function home()
+    {
+        $messages = [
+            [
+                'id' => 1,
+                'content' => 'Este es mi primer mensaje!',
+                'image' => 'http://lorempixel.com/600/338?1'
+            ],
+            [
+                'id' => 2,
+                'content' => 'Este es mi segundo mensaje!',
+                'image' => 'http://lorempixel.com/600/338?2'
+            ],
+                [
+                    'id' => 3,
+                    'content' => 'Este es otro mensaje!',
+                    'image' => 'http://lorempixel.com/600/338?3'
+                ],
+                [
+                    'id' => 4,
+                    'content' => 'Este es el ultimo mensaje!',
+                    'image' => 'http://lorempixel.com/600/338?4'
+                ],
+        ];
+
+        return view('welcome', [
+            'messages' => $messages,
+        ]);
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron text-center">
+        <h1>Laratter</h1>
+        <nav>
+            <ul class="nav nav-pills">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <div class="row">
+        @forelse ($messages as $message)
+            <div class="col-6">
+            <img class="img-thumbnail" src="{{ $message['image'] }}">
+            <p class="card-text">
+                {{ $message['content'] }} 
+            <a href="/messages/{{ $message['id'] }}">Leer más</a>
+            </p>
+            </div>   
+        @empty
+        <p>No hay mensajes destacados</p>
+        @endforelse
+    </div>
+@endsection
+
